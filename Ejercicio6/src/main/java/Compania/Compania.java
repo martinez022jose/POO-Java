a3200a9d912ee48331be12d3b93f543c01fd6bfc
Jose_Martinez <martinez022jose@gmail.com>
2020-08-16 11:28:43 -0300

    Se modifica punto 5/6

--- Ejercicio6/src/main/java/Compania/Compania.java
+++ Ejercicio6/src/main/java/Compania/Compania.java
@@ -5,10 +5,33 @@
  */
 package Compania;
 
+import Individuo.*;
+import Individuo.Individuo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 /**
  *
  * @author Jose
  */
 public class Compania {
+    private List<Individuo> empleados;
     
+    public Compania(){
+        this.empleados = new ArrayList<>();
+    }
+    
+    public List<Individuo> getEmpleados(){
+        return this.empleados;
+    }
+    
+    public List<Individuo> enfrentamientoMasDuros(){
+        return this.empleados.stream().map(empleado->empleado.enfrentamientoMasDuro()).collect(Collectors.toList());
+    }
+
+    public int totalPotencia(){
+        return this.empleados.stream().filter(empleado->empleado.esDeConfianza()).collect(Collectors.toList()).stream().mapToInt(empleado->empleado.getPotencia()).sum();
+    }
 }
